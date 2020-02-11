47abd57dc89d206b192e2aef8b7d8c09661ca12b
nabilHamrouni1991 <Hamrouni.nabil1991@gmail.com>
2020-02-11 10:37:36 +0100

    first commit

---
+++ src/Components/Box.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Box.css";
+
+function Box({ boxArray }) {
+    return (
+      <div className="carre"> 
+          {boxArray.map((el,i)=>(
+             
+          <div className="carre1">
+              <img src={el.pic}/>
+              <h3 className="titleBox">{el.title}</h3>
+               <p> {el.tag}</p> 
+
+           </div>
+          ))}
+      </div>
+    );
+  }
+
+  export default Box;
+  
---
+++ src/Components/Card.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Box.css";
+
+
+function Card({boxCard}) {
+    return (
+      <div className="Pic">
+          {boxCard.map((el,i)=>(
+        <div >
+          <img src={el.pic}/>
+          <div className="btn">
+          <p>{el.title}</p>
+          <button>See openning</button>
+          </div>
+      </div>
+
+          ))}
+          </div>
+    );
+    
+  }
+
+  export default Card;
